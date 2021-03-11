bba4b3633b4a3451e6519450e1c7a28edb311df6
GGwp-ctrl <for-games.04@yandex.ru>
2021-03-11 10:47:31 +0300

    add error_5

--- errors/error_5.py
+++ errors/error_5.py
@@ -5,8 +5,10 @@ def error_5(filename):
     with open(filename) as filename:
         cash = filename.read()
         cash = cash.split('\n')
-        if cash[-1].count('r') or cash[-1].count('R') or cash[-1].count('L') or cash[-1].count('c') or cash[-1].count(
-            'C'):
+        if cash[-1].count('r') + cash[-1].count('R') + cash[-1].count('L') + cash[-1].count('c') + cash[-1].count(
+                'C') == 1:
             return True
         return False
 
+
+print(error_5('qwe'))
