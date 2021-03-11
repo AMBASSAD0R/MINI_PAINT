ab386f4b3ab99ead53d4d4ed9158fb4f32708748
Администратор <i-l1ke-food@yandex.ru>
2021-03-11 11:25:44 +0300

    fdfvdcvdvfgv

--- errors/error_1.py
+++ errors/error_1.py
@@ -1,7 +1,8 @@
 from additional_function import *
+from errors.error_2 import *
 
 
-def error_1(path):
+def error_1(path, file_name):
     if ft_len(ft_split(ft_split(path, '.')[1])) <= 2:
-        return True
+        return error_2(file_name)
     return False
--- errors/error_3.py
+++ errors/error_3.py
@@ -1,7 +1,10 @@
+from errors.error_4 import *
+
 def error_3(path):
     try:
         with open(path, mode='r', encoding='utf-8') as file:
             cash = file.read()
+            return error_4(path)
     except:
         print(f'Error: Operation file corrupted \n')
     file.close()
