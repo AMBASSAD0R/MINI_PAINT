8fbfe1604e8a16835f79c46118cb8a9806b24723
Администратор <i-l1ke-food@yandex.ru>
2021-03-11 10:57:55 +0300

    errror_1

--- errors/error_1.py
+++ errors/error_1.py
@@ -2,6 +2,6 @@ from additional_function import *
 
 
 def error_1(path):
-    if ft_len(ft_split(ft_split(path, '.')[1])[0]) == 2:
+    if ft_len(ft_split(ft_split(path, '.')[1])) <= 2:
         return True
     return False
