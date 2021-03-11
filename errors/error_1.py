52e5aa5c106140a86395667100b5a3e390199704
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2021-03-11 11:53:40 +0300

    Update error_1.py

--- errors/error_1.py
+++ errors/error_1.py
@@ -1,10 +1,11 @@
+import os
+
 from additional_function import *
-from errors.error_2 import *
+
+path = os.path.abspath('operation.it')
 
 
-def error_1(path, file_name):
-    if ft_len(ft_split(ft_split(path, '.')[1])) <= 2:
-        return error_2(file_name)
+def error_1(path):
+    if ft_len(ft_split(path, '.')[1]) == 1:
+        return True
     return False
-
-print(error_1())
