29f592f66db3a5cb20d108241e050e690cdee837
GGwp-ctrl <for-games.04@yandex.ru>
2021-03-11 11:22:46 +0300

    add error_8

--- errors/error_6.py
+++ errors/error_6.py
@@ -2,4 +2,4 @@ def error_6(filename):
     with open(filename) as filename:
         cash = filename.read()
         cash = cash.split('\n')
-        return len(cash[0].split(' ')) == 3 and len(cash[-1].split(' ')) <= 6
+    return len(cash[0].split(' ')) == 3 and len(cash[-1].split(' ')) <= 6
