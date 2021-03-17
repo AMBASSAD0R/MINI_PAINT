8b644d7ec73d63cc631df1f0cb81101d5238d86c
Tratut <trataty111@gmail.com>
2021-03-17 22:43:52 +0300

    Норм обработчик

--- Creat_picture.py
+++ Creat_picture.py
@@ -20,17 +20,40 @@ def prinal_islam(mas_picture):  # ну типа обрезает -______-
     Запихивает картинкy в основной массив
     """
     global mas_global
-    x_len = ft_len(mas_global[0])
-    y_len = ft_len(mas_global)
 
-    for y in range(y_len):
-        for x in range(x_len):
-            if mas_picture[y][x] != '':
-                mas_global[y][x] = mas_picture[y][x]
+    if ft_len(mas_picture[0]) >= ft_len(mas_global[0]) and ft_len(mas_picture) >= ft_len(mas_global):
+        x_len = ft_len(mas_global[0])
+        y_len = ft_len(mas_global)
+        for y in range(y_len):
+            for x in range(x_len):
+                if mas_picture[y][x] != '':
+                    mas_global[y][x] = mas_picture[y][x]
+    elif ft_len(mas_picture[0]) >= ft_len(mas_global[0]) or ft_len(mas_picture) >= ft_len(mas_global):
+        if ft_len(mas_picture[0]) >= ft_len(mas_global[0]):
+            x_len = ft_len(mas_global[0])
+            y_len = ft_len(mas_picture)
+            for y in range(y_len):
+                for x in range(x_len):
+                    if mas_picture[y][x] != '':
+                        mas_global[y][x] = mas_picture[y][x]
+        else:
+            x_len = ft_len(mas_picture[0])
+            y_len = ft_len(mas_global)
+            for y in range(y_len):
+                for x in range(x_len):
+                    if mas_picture[y][x] != '':
+                        mas_global[y][x] = mas_picture[y][x]
+    elif ft_len(mas_picture[0]) < ft_len(mas_global[0]) and ft_len(mas_picture) < ft_len(mas_global):
+        x_len = ft_len(mas_picture[0])
+        y_len = ft_len(mas_picture)
+        for y in range(y_len):
+            for x in range(x_len):
+                if mas_picture[y][x] != '':
+                    mas_global[y][x] = mas_picture[y][x]
 
 
-create_mas(5, 8, '_')
-xx = square_not_full(1, 3, 4, 4, '*')
+create_mas(3, 6, '_')
+xx = square_not_full(0, 0, 4, 4, '*')
 prinal_islam(xx)
 for x in range(len(mas_global)):
     print(*mas_global[x])
