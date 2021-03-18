67195756248d40468dea01447a9ef39952366f76
GGwp-ctrl <for-games.04@yandex.ru>
2021-03-18 11:38:10 +0300

    delete error_7.py

--- errors/error_6.py
+++ errors/error_6.py
@@ -1,4 +1,3 @@
-from errors.error_7 import error_7
 from errors.error_8 import *
 
 
@@ -7,6 +6,6 @@ def error_6(path):
         data = file.read()
         data = ft_split(data, '\n')
     if ft_len(ft_split(data[0])) == 3 and 5 <= ft_len(ft_split(data[-1])) <= 6:
-        return error_7(path)
+        return error_8(path)
     print('Error6')
     return False
